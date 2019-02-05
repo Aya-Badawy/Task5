51daa41b1e4140d2efd60101a56238c9dbb802d0
ayabadawy <ayaahmed20az@gmail.com>
2019-02-05 19:59:20 +0200

    #106 [folder , css, jquery,bootstrap,css,js] admin panel required style files to public

--- resources/views/ad_dashboard.blade.php
+++ resources/views/ad_dashboard.blade.php
@@ -14,9 +14,9 @@
   <title>Admin panel</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="../dashboard_style/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{  app_path() . '/dashboard_style/bower_components/bootstrap/dist/css/bootstrap.min.css'}}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../dashboard_style/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../ashboard_style/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="../dashboard_style/bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
@@ -71,7 +71,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="../dashboard_style/dist/img/avatar_img.png" class="img-circle" alt="User Image">
+          <img src="dashboard_style/dist/img/avatar_img.png" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Admin name</p>
